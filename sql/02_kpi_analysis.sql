910f737564e7c8782fbc0c523c76253b50d69494
grittajoshy <156193304+grittalikestocode@users.noreply.github.com>
2026-03-06 16:56:25 +0530

    Create 02_kpi_analysis.sql

---
+++ sql/02_kpi_analysis.sql
@@ -0,0 +1,57 @@
+-- Core KPIs
+SELECT
+  (SELECT SUM(revenue) FROM ecommerce_analysis.transactions) AS total_revenue,
+
+  (SELECT COUNT(*) FROM ecommerce_analysis.transactions) AS total_orders,
+
+  (SELECT COUNT(DISTINCT user_pseudo_id)
+   FROM ecommerce_analysis.sessions) AS total_users
+
+-- AOV using session revenue
+
+SELECT
+  SUM(session_revenue) AS total_revenue,
+  COUNTIF(session_revenue > 0) AS total_orders,
+  SAFE_DIVIDE(
+    SUM(session_revenue),
+    COUNTIF(session_revenue > 0)
+  ) AS avg_order_value
+FROM `ecommerce_analysis.sessions`;
+
+-- AOV using transactions
+SELECT
+  COUNT(*) AS total_transactions,
+  SUM(revenue) AS total_revenue,
+  SAFE_DIVIDE(SUM(revenue), COUNT(*)) AS avg_order_value
+FROM ecommerce_analysis.transactions;
+
+-- Session conversion rate 
+
+SELECT
+  COUNT(DISTINCT session_id) AS total_sessions,
+  COUNTIF(purchases > 0) AS converting_sessions,
+  SAFE_DIVIDE(
+    COUNTIF(purchases > 0),
+    COUNT(DISTINCT session_id)
+  ) AS conversion_rate
+FROM `ecommerce_analysis.sessions`;
+
+--Repeat Purchase Rate
+WITH customer_orders AS (
+  SELECT
+    user_pseudo_id,
+    COUNT(*) AS total_orders
+  FROM ecommerce_analysis.transactions
+  GROUP BY user_pseudo_id
+)
+
+SELECT
+  COUNTIF(total_orders > 1) AS repeat_customers,
+  COUNT(*) AS total_customers,
+  SAFE_DIVIDE(
+    COUNTIF(total_orders > 1),
+    COUNT(*)
+  ) AS repeat_purchase_rate
+FROM customer_orders;
+
+
